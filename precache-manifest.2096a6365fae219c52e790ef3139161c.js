4d68aa77cf824e7d264083c5afa186ef05c07e82
MacBook di Enrico <Enrico@MacBook-di-MacBook.local>
2019-03-13 14:13:13 +0900

    Updates

---
+++ precache-manifest.2096a6365fae219c52e790ef3139161c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0428cd74552ae1441617",
+    "url": "/robo-app/static/js/runtime~main.0428cd74.js"
+  },
+  {
+    "revision": "4822e8c8124961c3565b",
+    "url": "/robo-app/static/js/main.4822e8c8.chunk.js"
+  },
+  {
+    "revision": "f538d8352fa53e6aafe5",
+    "url": "/robo-app/static/js/2.f538d835.chunk.js"
+  },
+  {
+    "revision": "4822e8c8124961c3565b",
+    "url": "/robo-app/static/css/main.b9bec14b.chunk.css"
+  },
+  {
+    "revision": "f538d8352fa53e6aafe5",
+    "url": "/robo-app/static/css/2.d3853571.chunk.css"
+  },
+  {
+    "revision": "a5fdee4d11eeb5487c32814198717e02",
+    "url": "/robo-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/robo-app/precache-manifest.2096a6365fae219c52e790ef3139161c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/robo-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
