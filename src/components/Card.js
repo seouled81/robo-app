bf35f26d5e7d1590064306aaeba51c1e73c819e9
MacBook di Enrico <Enrico@MacBook-di-MacBook.local>
2019-03-13 14:49:52 +0900

    add 250 px robot

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const card = ({id, name, email}) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?size=400x400`} alt="robot img"/>
+      <img src={`https://robohash.org/${id}?size=300x300`} alt="robot img"/>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
